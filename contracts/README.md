fafdedfbd733a1f3b53b60e86973a4ed3312e8e1
Jason Sun <sunapi386@gmail.com>
2023-09-23 21:29:02 -0400

    lilypad stuff

--- contracts/README.md
+++ contracts/README.md
@@ -11,3 +11,5 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+
+https://github.com/bacalhau-project/lilypad/tree/main/hardhat
--- contracts/contracts/PaymentGateway.sol
+++ contracts/contracts/PaymentGateway.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract PaymentGateway {
+// Import the LilypadCallerInterface
+// https://github.com/bacalhau-project/lilypad/tree/main/hardhat
+import "./LilypadCallerInterface.sol";
+
+contract PaymentGateway is LilypadCallerInterface {
     address public owner; // Owner of the contract
     address public backendAddress; // Address of the backend authorized to make payments
 
@@ -20,7 +24,9 @@ contract PaymentGateway {
         require(msg.sender == backendAddress, "Only the backend can make payments");
         require(address(this).balance >= amount, "Insufficient balance in the contract");
 
-        recipient.transfer(amount);
+        // Call the LilypadEvents contract to trigger a job
+        LilypadEvents lilypadEvents = LilypadEvents(0xMyLilypadEventsContractAddress);
+        lilypadEvents.runLilypadJob("YourDockerSpecJSONData", address(this), amount);
     }
 
     // Function to withdraw funds from the contract (only the owner)
@@ -35,5 +41,20 @@ contract PaymentGateway {
     function setBackendAddress(address newBackendAddress) external {
         require(msg.sender == owner, "Only the owner can change the backend address");
         backendAddress = newBackendAddress;
+    }
+
+    // Implement the lilypadFulfilled callback function
+    function lilypadFulfilled(
+        address _from,
+        uint256 _jobId,
+        LilypadResultType _resultType,
+        string calldata _result
+    ) external override {
+        // Handle the successful completion of the Lilypad job here
+    }
+
+    // Implement the lilypadCancelled callback function
+    function lilypadCancelled(address _from, uint256 _jobId, string calldata _errorMsg) external override {
+        // Handle the cancellation or failure of the Lilypad job here
     }
 }
